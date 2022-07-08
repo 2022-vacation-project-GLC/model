48acd6605c4f94ed69fefe90619e1531448cb676
10eace <10eacemkr@gmail.com>
2022-07-08 13:27:17 +0900

    change directory name

--- DLS_git_clone/common/layers.py
+++ DLS_git_clone/common/layers.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import numpy as np
-from common.functions import *
-from common.util import im2col, col2im
+from DLS_git_clone.common.functions import *
+from DLS_git_clone.common.util import im2col, col2im
 
 
 class Relu:
